a3f0806cb32401ad8656f6ad1fcbfb00031a74de
Raghav Deedwaniya <108008288+raghavdeedwaniya@users.noreply.github.com>
2025-04-16 21:02:11 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,61 @@
+import { Container } from './styles'
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import whatsapp from '../../assets/whatsapp.svg'
+import telegram from '../../assets/telegram.svg'
+import instagramIcon from '../../assets/instagram.svg'
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      <a href="https://linktr.ee/Raghavdeedwaniya" className="logo">
+        <span>www.RaghavDeedwaniya</span>
+        <span>.in</span>
+      </a>
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+          {/* <span>❤️</span> */}
+        </p>
+      </div>
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/raghavdeedwaniya/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/raghavdeedwaniya"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        <a
+          // href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak+I+found+your+contact+through+portfolio+site.%0A%0A"
+          // target="_blank"
+          // rel="noreferrer"
+        >
+          <img src={whatsapp} alt="Whatsapp" />
+        </a>
+        <a
+          // href="https://t.me/CodeVinayak"
+          // target="_blank"
+          // rel="noreferrer"
+        >
+          <img src={telegram} alt="telegram" />
+        </a>
+        <a
+          href="https://www.instagram.com/raghav_deedwaniya"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+      </div>
+    </Container>
+  )
+}
